5b1cc52c70545ae8451d35778b3ceb024ad56d99
Vitor Soares <pereiravitor1218@gmail.com>
2022-10-21 18:51:40 -0300

    test

--- src/tests/unit/controllers/car.test.ts
+++ src/tests/unit/controllers/car.test.ts
@@ -19,6 +19,7 @@ describe('Car Controller', () => {
   before(async () => {
     sinon.stub(carService, 'create').resolves(mockCarWithId);
     sinon.stub(carService, 'read').resolves([mockCarWithId]);
+    sinon.stub(carService, 'readOne').resolves(mockCarWithId);
 
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
@@ -43,6 +44,17 @@ describe('Car Controller', () => {
       await carController.read(req, res);
       expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
       expect((res.json as sinon.SinonStub).calledWith([mockCarWithId])).to.be
+        .true;
+    });
+  });
+
+  describe('ReadOne Car', () => {
+    it('Success', async () => {
+      req.params = { id: mockCarWithId._id };
+      await carController.readOne(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(mockCarWithId)).to.be
         .true;
     });
   });
--- src/tests/unit/services/car.test.ts
+++ src/tests/unit/services/car.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import { ZodError } from 'zod';
+import { ErrorTypes } from '../../../errors/catalog';
 import CarModel from '../../../models/Car';
 import CarService from '../../../services/Car';
 import { mockCar, mockCarWithId } from '../../mocks/car';
@@ -11,6 +12,12 @@ describe('Car service', () => {
   before(async () => {
     sinon.stub(carModel, 'create').resolves(mockCarWithId);
     sinon.stub(carModel, 'read').resolves([mockCarWithId]);
+    sinon
+      .stub(carModel, 'readOne')
+      .onCall(0)
+      .resolves(mockCarWithId)
+      .onCall(1)
+      .resolves(null);
   });
 
   after(() => {
@@ -38,6 +45,24 @@ describe('Car service', () => {
       const cars = await carService.read();
       expect(cars).to.be.an('array');
       expect(cars).to.be.deep.equal([mockCarWithId]);
+    });
+  });
+
+  describe('readOne', () => {
+    it('Sucess', async () => {
+      const cars = await carService.readOne('60e9b4f0f3f7b8b9b8b9b8b9');
+      expect(cars).to.be.deep.equal(mockCarWithId);
+    });
+
+    it('Invalid id', async () => {
+      let error;
+      try {
+        await carService.readOne('invalid id');
+      } catch (err: any) {
+        console.log(err);
+        error = err;
+      }
+      expect(error.message).to.be.deep.equal(ErrorTypes.InvalidMongoId);
     });
   });
 });
