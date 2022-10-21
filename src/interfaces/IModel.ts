cdebe59dc15c7a24a13cfe35d3b9763942d5f71d
Vitor Soares <pereiravitor1218@gmail.com>
2022-10-21 14:50:35 -0300

    Feat: interface IModel

---
+++ src/interfaces/IModel.ts
@@ -0,0 +1,9 @@
+interface IModel<T> {
+  create: (obj: T) => Promise<T>;
+  read: () => Promise<T[]>;
+  readOne: (id: string) => Promise<T | null>;
+  update: (id: string, obj: T) => Promise<T | null>;
+  delete: (id: string) => Promise<T | null>;
+}
+
+export { IModel };
