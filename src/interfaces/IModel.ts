ff7e70b3c62c967611cc3e48ab82cad0ceacff8c
Vitor Soares <pereiravitor1218@gmail.com>
2022-10-21 16:10:28 -0300

    feat: Model Car

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -2,7 +2,7 @@ interface IModel<T> {
   create: (obj: T) => Promise<T>;
   read: () => Promise<T[]>;
   readOne: (id: string) => Promise<T | null>;
-  update: (id: string, obj: T) => Promise<T | null>;
+  update: (id: string, obj: Partial<T>) => Promise<T | null>;
   delete: (id: string) => Promise<T | null>;
 }
 
