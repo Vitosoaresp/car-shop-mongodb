b56c82ebb64d44a053a874b697fd0246d0738454
Vitor Soares <pereiravitor1218@gmail.com>
2022-10-21 17:05:01 -0300

    feat: implements index.ts and app use carRoute

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+
+import errorHandler from './middleware/error';
+import carRoutes from './routes/car';
 
 const app = express();
+
+app.use(express.json());
+app.use(carRoutes);
+app.use(errorHandler);
 
 export default app;
