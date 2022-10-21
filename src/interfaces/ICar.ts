34ebd115015030fc1ef922732d0f8ea130eb976b
Vitor Soares <pereiravitor1218@gmail.com>
2022-10-21 17:03:39 -0300

    change schema zod car

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,11 +1,15 @@
 import { z } from 'zod';
-import { IVehicle } from './IVehicle';
 
 const carZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().min(1900).max(2021),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
   doorsQty: z.number().int().min(2).max(4),
   seatsQty: z.number().int().min(2).max(7),
 });
 
-type ICar = IVehicle & z.infer<typeof carZodSchema>;
+type ICar = z.infer<typeof carZodSchema>;
 
 export { ICar, carZodSchema };
