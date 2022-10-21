322828cf16b3a794329a454f8c40704043d2513e
Vitor Soares <pereiravitor1218@gmail.com>
2022-10-21 16:03:54 -0300

    feat: read and readOne

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -1,4 +1,4 @@
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { IModel } from '../interfaces/IModel';
 
 abstract class MongoModel<T> implements IModel<T> {
@@ -6,6 +6,15 @@ abstract class MongoModel<T> implements IModel<T> {
 
   constructor(model: Model<T>) {
     this._model = model;
+  }
+
+  public read(): Promise<T[]> {
+    return this._model.find().exec();
+  }
+
+  public readOne(id: string): Promise<T | null> {
+    if (!isValidObjectId(id)) throw Error('Invalid ID');
+    return this._model.findById(id).exec();
   }
 
   public create(obj: T): Promise<T> {
