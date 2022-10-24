8eca02b4149a1e613a906f3f345c6749a42e4017
Vitor Soares <pereiravitor1218@gmail.com>
2022-10-24 16:38:11 -0300

    feat: carService update and readOne

--- src/services/Car.ts
+++ src/services/Car.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from 'mongoose';
 import { ErrorTypes } from '../errors/catalog';
 import { carZodSchema, ICar } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
@@ -21,9 +22,24 @@ export default class CarService implements IService<ICar> {
     return this._car.read();
   }
 
-  public readOne(_id: string): Promise<ICar | null> {
-    const car = this._car.readOne(_id);
-    if (!car) throw Error(ErrorTypes.EntityNotFound);
+  public async readOne(_id: string): Promise<ICar | null> {
+    if (_id.length < 24) throw new Error(ErrorTypes.InvalidLengthId);
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
+
+    const car = await this._car.readOne(_id);
+    if (!car) throw Error(ErrorTypes.InvalidMongoId);
+
+    return car;
+  }
+
+  public async update(_id: string, obj: ICar): Promise<ICar | null> {
+    if (_id.length < 24) throw new Error(ErrorTypes.InvalidLengthId);
+    if (JSON.stringify(obj) === '{}') throw new Error(ErrorTypes.InvalidBody);
+    const carParsed = carZodSchema.safeParse(obj);
+    if (!carParsed.success) throw carParsed.error;
+
+    const car = await this._car.update(_id, obj);
+    if (!car) throw Error(ErrorTypes.InvalidMongoId);
     return car;
   }
 }
